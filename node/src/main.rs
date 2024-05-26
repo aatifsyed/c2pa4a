754e41d1d1ab8cd1575790a50b00d8f56ad813ec
Aatif Syed <aatifsyedyp@gmail.com>
2024-05-26 02:58:04 +0200

    refactor: wrap

--- node/src/main.rs
+++ node/src/main.rs
@@ -1,80 +1,158 @@
+//! Getting [`c2pa`] to work in-memory was too much for my little brain, so have
+//! a hacky server around it instead.
+
 use std::{
-    fs,
+    fs::{self, File},
     io::{self, Cursor, Read, Write},
     path::PathBuf,
+    process::Command,
 };
 
-use anyhow::Context;
+use anyhow::{ensure, Context};
 use c2pa::{Manifest, Signer, SigningAlg};
 use clap::Parser;
 use oxhttp::{
     model::{Request, Response, Status},
     Server,
 };
-use rcgen::{CertificateParams, CertifiedKey, KeyPair, SignatureAlgorithm};
 use serde::{Deserialize, Serialize};
-use serde_json::{json, Value};
-
-#[derive(Serialize, Deserialize)]
-struct Assertion {
-    label: String,
-    data: Value,
-}
-
-#[derive(Serialize, Deserialize)]
-struct ExifGpsData {
-    lat: String,
-    long: String,
-}
+use serde_json::json;
 
 #[derive(Parser)]
 struct Args {
     #[arg(long)]
-    write_pem: Option<PathBuf>,
+    ext: SupportedFileExtension,
     #[arg(long)]
     lat: f64,
     #[arg(long)]
-    long: f64,
+    lon: f64,
+    path: PathBuf,
 }
 
 fn main() -> anyhow::Result<()> {
     let Args {
-        write_pem,
+        ext,
         lat,
-        long,
+        lon,
+        path,
     } = Args::parse();
-    let alg = SigningAlg::Es256;
-    let key_pair = KeyPair::generate_for(&rcgen::PKCS_ECDSA_P256_SHA256)?;
-    let cert =
-        CertificateParams::new([String::from("C2PA4A Ephemeral")])?.self_signed(&key_pair)?;
-    if let Some(write_pem) = write_pem {
-        fs::write(write_pem, cert.pem())?;
-    }
-    let signer = c2pa::create_signer::from_keys(
-        include_bytes!("../root.cert"),
-        include_bytes!("../root.key"),
-        alg,
-        None,
-    )?;
+    io::stdout().write_all(&with_attestations(File::open(path)?, ext, lat, lon)?)?;
+    Ok(())
+}
 
-    let mut manifest = serde_json::from_value::<Manifest>(json!({
-        "assertions": [
-            {
-                "label": "stds.exif",
-                "data": {
-                    "EXIF:GPSLatitude": lat,
-                    "EXIF:GPSLongitude": long,
+fn with_attestations(
+    mut input: impl io::Read,
+    format: SupportedFileExtension,
+    lat: f64,
+    lon: f64,
+) -> anyhow::Result<Vec<u8>> {
+    let suffix = format!(".{}", format);
+    let mut shim_input = tempfile::Builder::new().suffix(&suffix).tempfile()?;
+    io::copy(&mut input, &mut shim_input)?;
+
+    let shim_output = tempfile::Builder::new()
+        .suffix(&suffix)
+        .tempfile()?
+        .into_temp_path();
+
+    let mut cmd = Command::new("c2patool");
+    cmd.arg(shim_input.path())
+        .arg("--config")
+        .arg(
+            json!({"assertions": [
+                {
+                    "label": "stds.exif",
+                    "data": {
+                        "EXIF:GPSLatitude": lat,
+                        "EXIF:GPSLongitude": lon,
+                    }
+                }
+            ]})
+            .to_string(),
+        )
+        .arg("--output")
+        .arg(&shim_output)
+        .arg("--force")
+        .stdout(io::stderr());
+    dbg!(&cmd);
+    let status = cmd.status()?;
+    ensure!(status.success());
+
+    Ok(fs::read(shim_output)?)
+}
+
+macro_rules! strum {
+    (
+        $(#[$enum_meta:meta])*
+        $vis:vis enum $enum_name:ident {
+            $(
+                $(#[$variant_meta:meta])*
+                $variant_name:ident = $string:literal
+            ),* $(,)?
+        }
+    ) => {
+        $(#[$enum_meta])*
+        $vis enum $enum_name {
+            $(
+                $(#[$variant_meta])*
+                $variant_name,
+            )*
+        }
+        impl $enum_name {
+            pub fn as_str(&self) -> &'static str {
+                match *self {
+                    $(
+                        Self::$variant_name => $string,
+                    )*
                 }
             }
-        ]
-    }))?;
+        }
+        impl core::fmt::Display for $enum_name {
+            fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+                f.write_str(self.as_str())
+            }
+        }
+        impl core::str::FromStr for $enum_name {
+            type Err = ParseError;
+            fn from_str(s: &str) -> Result<Self, Self::Err> {
+                match s {
+                    $(
+                        $string => Ok(Self::$variant_name),
+                    )*
+                    _ => Err(ParseError(()))
+                }
+            }
+        }
+        #[derive(Debug)]
+        $vis struct ParseError(());
+        impl core::fmt::Display for ParseError {
+            fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+                f.write_str("couldn't parse")
+            }
+        }
+        impl std::error::Error for ParseError {}
+    };
+}
 
-    let mut i = Cursor::new(vec![]);
-    io::stdin().read_to_end(i.get_mut())?;
-
-    let mut o = Cursor::new(vec![]);
-    manifest.embed("/dev/stdin", "/dev/stdout", &*signer)?;
-
-    io::stdout().write_all(o.get_ref())?;
-    Ok(())
+strum! {
+    #[derive(Clone)]
+    pub enum SupportedFileExtension {
+        Avi = "avi",
+        Avif = "avif",
+        C2pa = "c2pa",
+        Dng = "dng",
+        Heic = "heic",
+        Heif = "heif",
+        Jpg = "jpg",
+        M4a = "m4a",
+        Mp3 = "mp3",
+        Mp4 = "mp4",
+        Mov = "mov",
+        Pdf = "pdf",
+        Png = "png",
+        Svg = "svg",
+        Tif = "tif",
+        Wav = "wav",
+        Webp = "webp",
+    }
 }
